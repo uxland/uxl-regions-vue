ccb2d6959b3c24c6cefe4ec98820d89ac05a7da0
Alex Vizcaino <avizcaino@uxland.es>
2019-03-04 10:51:49 +0100

    fix: add demo

--- test/unit/behaviors/default-registry.spec.ts
+++
@@ -1,9 +0,0 @@
-import {expect} from 'chai';
-import {defaultBehaviorRegistry} from "../../../src/behaviors/default-registry";
-import {AutoPopulateBehavior} from "../../../src/behaviors/auto-populate-behavior";
-
-describe('Given default registry', () =>{
-    it('should contain AutoPopulateBehavior', () =>{
-        expect(defaultBehaviorRegistry.behaviors).to.contain(AutoPopulateBehavior);
-    })
-})
